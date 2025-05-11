e4ec8349bc3e6a658fa91cb1fb09948b47f1f529
reonaore <reonaore@users.noreply.github.com>
2025-05-11 13:42:18 +0000

    deploy: dad8e7ac71e5dbcc80eef8824f9a40e563d35ad8

--- scripts/sourceset_dependencies.js
+++ scripts/sourceset_dependencies.js
@@ -1,1 +1,1 @@
-sourceset_dependencies='{":dokkaHtml/main":[]}'
+sourceset_dependencies='{":ulidk-core:dokkaHtml/commonMain":[],":ulidk-core:dokkaHtml/jsMain":[":ulidk-core:dokkaHtml/commonMain"],":ulidk-core:dokkaHtml/jvmMain":[":ulidk-core:dokkaHtml/commonMain"]}'
